ede8b06794c52faae0a5e1cc49d9fb8fda69ae5d
mehrdadhadifar <mlp8512@gmail.com>
2020-09-29 18:10:59 +0330

    Initial commit

---
+++ src/main/java/com/hfad/musighy/MusicPagerActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.musighy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_pager);
+    }
+}
